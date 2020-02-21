6071ecd88a50730fd33c07b50acf6850e90eb1e2
liquidz <liquidz.uo@gmail.com>
2020-02-22 07:41:22 +0900

    Update description and url

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject esac "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Advanced \"case\" library for Clojure(Script)"
+  :url "https;//github.com/liquidz/esac"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
 
